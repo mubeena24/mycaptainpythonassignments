4d53f7440988553e3ab732245b6b880be28b9516
mubeena24 <82866451+mubeena24@users.noreply.github.com>
2022-06-20 11:29:16 +0530

    Add files via upload

---
+++ assgn1_a.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 20 11:12:02 2022
+
+@author: MD Jahangeer
+"""
+
+radius = float(input("Enter the radius "))
+
+area = 3.14159265358979*radius*radius
+
+
+
+
+print("the area of the circle with radius",radius " is ", area)
